df761d6836135dbcf19c58ad54ac349a85387e71
kamilrzesz <rzeszotk@uni.coventry.ac.uk>
2016-11-17 00:31:48 +0000

    inside_farm() and go_to_start_field() functions

--- mainTk.py
+++ mainTk.py
@@ -5,23 +5,40 @@ def move_widget(item, x, y):
     """Moves a widget(item) on the main canvas to the new coordinates(x,y)"""
     main_canvas.coords(item,x,y)
     main_canvas.update()
-def plough_field(x,y, l, w):
+def add_cabbage_field(x,y, l, w,item):
     """ Top left conner of the farm is at coordinates (x,y)
         Creates a farm with the width of 'w' and length of 'l'
         l and w has to be in multiples of 10 """
-    global ploughed_texture
-    ploughed_texture = PhotoImage(file="textures/cabbage.gif")
     for i in range(x,l+x,10):
         for k in range(y,w+y,10):
-            main_canvas.create_image(i,k,image=ploughed_texture, anchor = NW)
+            main_canvas.create_image(i,k,image=item, anchor = NW)
+    existing_farms_x1.append(x)
+    existing_farms_x2.append(x+l)
+    existing_farms_y1.append(y)
+    existing_farms_y2.append(y+w)
+    print(existing_farms_x1,existing_farms_x2,existing_farms_y1,existing_farms_y2)
+def inside_farm(tractor):
+    x,y= main_canvas.coords(tractor)
+    i = 0
+    while i < len(existing_farms_x1):
+        if x > existing_farms_x1[i] and x< existing_farms_x2[i] and y > existing_farms_y1[i] and y<existing_farms_y2[i]:
+            go_start_field(tractor, existing_farms_x1[i],existing_farms_y1[i])
+            return True
+        else:
+            status = False
+        i+=1
+    return status
+def go_start_field(tractor, farm_x, farm_y):
+    move_widget(tractor,farm_x,farm_y)
 def main():
     root = Tk()
     icon = Image("photo", file="textures/tractor.gif")  
     root.tk.call('wm','iconphoto',root._w,icon) #Changes the application icon
+    global main_canvas
     main_canvas = Canvas(width =600, height = 400, bg='white')#create canvas
     main_canvas.pack(expand = YES, fill = BOTH)
     bck_img = PhotoImage(file="textures/background.gif")
-    global main_canvas
+    main_canvas.create_image(0,0,image=bck_img,anchor=NW) # Sets background of the window to grass
     main_canvas.create_image(0,0,image=bck_img,anchor=NW) # Sets background of the window to grass
     tractor_img = PhotoImage(file="textures/tractor.gif")
     x_min = 0 # min and max values used in setting boundaries on the canvas
@@ -30,14 +47,18 @@ def main():
     y_max=int(main_canvas['height'])
     rh= randint(8,12)
     rw = randint(10,14)
-    plough_field(300,220,int(rw*10),int(rh*10))
+    global existing_farms_x1,existing_farms_x2, existing_farms_y1, existing_farms_y2
+    cabbage_texture = PhotoImage(file="textures/cabbage.gif")
+    existing_farms_x1,existing_farms_x2, existing_farms_y1, existing_farms_y2 = [],[],[],[]
+    add_cabbage_field(100,50,int(rw*10),int(rh*10),cabbage_texture)
+    add_cabbage_field(300,220,int(rw*10),int(rh*10),cabbage_texture)
+    print(existing_farms_x1)
+    tractor_img = PhotoImage(file="textures/tractor.gif")
     tractor1 = main_canvas.create_image(300,200,image=tractor_img, anchor = NW) #adding tractor to the canvas
     vx = 1
     vy = 1
     while True:
-        x1,y1= main_canvas.coords(tractor1)  # takes current coordinates of tractor
-        if x1 > 300 and x1<((rw*10)+300)and y1 > 220 and y1<((rh*10)+220):
-            print("inside farm") # checks if the x coord of tractor is inside field
+        x1,y1= main_canvas.coords(tractor1)
         if (x1+10)> x_max:
             vx=-1
         if (y1+10)> y_max:
@@ -47,6 +68,7 @@ def main():
         if (x1)<x_min:
             vx=1
         move_widget(tractor1, x1+vx, y1+vy)
+        inside_farm(tractor1)
         time.sleep(0.01)
     mainloop()
 main()

9174bc88ed6454916a5f546c27f981d60d7e764b
yeamin-s <yrahman98@gmail.com>
2016-11-18 13:25:23 +0000

    Added sheep button

--- mainTk.py
+++ mainTk.py
@@ -39,6 +39,7 @@ def inside_farm(tractor):
             status = False
         i+=1
     return status
+
 def collect_cabbage(tractor,farm_x,farm_y, farm_x2, farm_y2,texture,trac_img):
     farm_length = farm_x2 - farm_x
     farm_height = farm_y2 - farm_y
@@ -53,6 +54,30 @@ def collect_cabbage(tractor,farm_x,farm_y, farm_x2, farm_y2,texture,trac_img):
             time.sleep(0.09)
     main_canvas.tag_raise(tractor)
     return cabbage_count
+
+def add_sheep (x,y,item):
+    '''This adds an image of a sheep to the canvas'''
+    main_canvas.create_image(x,y, image=item, anchor = NW,)
+
+    
+
+
+
+
+    
+    
+
+def fence(x,y,l,w,item):
+    """ Will make fence tileable (used in bottom of page) can be used later for
+    animal fencing etc.)"""
+    for a in range(x,l+x,10):
+        for b in range(y,w+y,10):
+            main_canvas.create_image(a,b,image=item,anchor = NW)
+
+def go_start_field(tractor, farm_x, farm_y):
+    move_widget(tractor,farm_x,farm_y)
+def sheep_button():
+    add_sheep(randint(1, 600), randint(1, 400), sheep1)
 def main():
     global root
     root = Tk()
@@ -83,6 +108,25 @@ def main():
     fence(1,5,int(600),int(10),fence_img)
     fence(1,1,int(10),int(400),side_fence)
     fence(595,1,int(10),int(400),side_fence)
+    global sheep1
+    sheep1=PhotoImage(file='textures/sheep.gif')
+
+
+
+    button = Button(main_canvas, width=30, text = 'Add Sheep', command = sheep_button)
+
+    button.configure(width=10, )
+    
+    button1 = main_canvas.create_window(1,1,anchor=NW,window=button)
+
+
+
+
+
+
+    
+    
+
     print(existing_farms_x1)
     tractor_img = PhotoImage(file="textures/tractor_right.gif")
     tractor1 = main_canvas.create_image(300,200,image=tractor_img, anchor = NW) #adding tractor to the canvas

f663351c64975e7f43f359bcada6cc4c6e6a3240
DrUSB <salimia2@uni.coventry.ac.uk>
2016-11-21 21:36:45 +0000

    Added farm add button
    I edited some comments and added the ADD Farm button to allow more
    farms to be added by the user

--- mainTk.py
+++ mainTk.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from random import randint
 import time
+
 def move_widget(item, x, y):
     """ Moves a widget(item) on the main canvas to the new coordinates(x,y)"""
     main_canvas.coords(item,x,y)
@@ -26,10 +27,10 @@ def fence(x,y,l,w,item):
             main_canvas.create_image(a,b,image=item,anchor = NW)
 def inside_farm(tractor):
     """ Checks if the tractor is inside the farm.
-        More specifcally it checks if the top left coordinate is inside the area marked by any of the cabbage fields 
+        More specifically it checks if the top left coordinate is inside the area marked by any of the cabbage fields
         on the screen, if they were added using add_cabbage_field function
         Returns a boolean result. True if tractor is inside a farm.
-        i is a global variable, sygnaling which farm the tractor is on, do not reassign else where"""
+        v is a global variable, signaling which farm the tractor is on, do not reassign else where"""
     x,y= main_canvas.coords(tractor)
     global v
     v = 0
@@ -68,6 +69,10 @@ def fence(x,y,l,w,item):
     for a in range(x,l+x,10):
         for b in range(y,w+y,10):
             main_canvas.create_image(a,b,image=item,anchor = NW)
+def farm_button():
+    '''Adds more farms when button is pressed'''
+    add_cabbage_field (randint(100, 1100),randint(50,600),(rw * 10), (rh * 10),cabbage_texture )
+
 def hay_button():
     add_hay(randint(50,1150), randint(50, 650),haybail)
 def go_start_field(tractor, farm_x, farm_y):
@@ -95,9 +100,12 @@ def main():
     print(barn)
     rh= randint(8,12)
     rw = randint(10,14)
+    global rh
+    global rw
     global existing_farms_x1,existing_farms_x2, existing_farms_y1, existing_farms_y2, existing_farms_type
     dirt_texture = PhotoImage(file="textures/dirt.gif")
     cabbage_texture = PhotoImage(file="textures/cabbage.gif")
+    global cabbage_texture
     existing_farms_x1,existing_farms_x2, existing_farms_y1, existing_farms_y2, existing_farms_type = [],[],[],[],[]
     add_cabbage_field(100,50,(rw*10),(rh*10),cabbage_texture)
     add_cabbage_field(300,220,(rw*10),(rh*10),cabbage_texture)
@@ -111,17 +119,20 @@ def main():
     global haybail
     button = Button(main_canvas, width=30, text = 'Add Sheep', command = sheep_button, bg='light green')
     button_haybail = Button(main_canvas, width=30, text = "Add Hay Bails", command=hay_button)
+    button_farm = Button(main_canvas, width=30, text = 'Add Farm',command=farm_button)
     button.configure(width=10)
     button_haybail.configure(width=10)
     button1 = main_canvas.create_window(1,(y_max)-30,anchor=NW,window=button)
     button2 = main_canvas.create_window(130,(y_max)-30,anchor=NW,window=button_haybail)
+    button3 = main_canvas.create_window(260,(y_max)-30,anchor=NW,window=button_farm)
     print(existing_farms_x1)
     tractor_img = PhotoImage(file="textures/tractor_right.gif")
     tractor1 = main_canvas.create_image(300,200,image=tractor_img, anchor = NW) #adding tractor to the canvas
     vx = -1
     vy = -1
     cabbages_global = 0
- 
+
+
     while True:
         x1,y1= main_canvas.coords(tractor1)
         if (x1+20)> x_max and inside_farm(tractor1)==False:
@@ -138,6 +149,8 @@ def main():
             continue
         if inside_farm(tractor1) == False or existing_farms_type[v] == "empty":
             move_widget(tractor1, x1+vx, y1+vy)
+
+
         time.sleep(0.01)
     mainloop()
 main()
